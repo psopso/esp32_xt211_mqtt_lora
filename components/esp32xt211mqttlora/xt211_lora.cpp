6e9e6889fd3bda273b656887ceb8e1e2663d1a09
psopso <57403373+psopso@users.noreply.github.com>
2026-04-09 14:37:27 +0200

    Update xt211_lora.cpp

--- components/esp32xt211mqttlora/xt211_lora.cpp
+++ components/esp32xt211mqttlora/xt211_lora.cpp
@@ -14,7 +14,8 @@ void MyComponent::setup() {
   ESP_LOGI(TAG, "SCK:  %d", sck_);
   ESP_LOGI(TAG, "NSS:  %d", nss_);
   ESP_LOGI(TAG, "RST:  %d", rst_);
-  ESP_LOGI(TAG, "DIO0: %d", dio0_);  setuprun = true;
+  ESP_LOGI(TAG, "DIO0: %d", dio0_);  
+  setuprun = true;
 }
 
 void MyComponent::loop() {
@@ -22,6 +23,13 @@ void MyComponent::loop() {
 
   if (now - last_log_time > 10000) {
     ESP_LOGI(TAG, "Loop běží %d", setuprun);
+    ESP_LOGI(TAG, "MOSI: %d", mosi_);
+    ESP_LOGI(TAG, "MISO: %d", miso_);
+    ESP_LOGI(TAG, "SCK:  %d", sck_);
+    ESP_LOGI(TAG, "NSS:  %d", nss_);
+    ESP_LOGI(TAG, "RST:  %d", rst_);
+    ESP_LOGI(TAG, "DIO0: %d", dio0_);  
+
     last_log_time = now;
   }
 }
