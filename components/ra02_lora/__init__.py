dd97ea6d4891c5a0028811140fc65d07b4f5b72d
psopso <57403373+psopso@users.noreply.github.com>
2026-04-09 23:18:00 +0200

    Update __init__.py

--- components/ra02_lora/__init__.py
+++ components/ra02_lora/__init__.py
@@ -22,5 +22,15 @@ async def to_code(config):
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config) # Propojí CS pin a SPI bus
     
-    cg.add(var.set_reset_pin(config["reset_pin"]))
-    cg.add(var.set_dio0_pin(config["dio0_pin"]))
+    if "reset_pin" in config:
+        # await cg.gpio_pin_expression vytvoří z konfigurace C++ objekt
+        reset_pin = await cg.gpio_pin_expression(config["reset_pin"])
+        cg.add(var.set_reset_pin(reset_pin))
+
+    if CONF_CS_PIN in config:
+        cs_pin = await cg.gpio_pin_expression(config[CONF_CS_PIN])
+        cg.add(var.set_cs_pin(cs_pin))
+
+    if "dio0_pin" in config:
+        dio0_pin = await cg.gpio_pin_expression(config["dio0_pin"])
+        cg.add(var.set_dio0_pin(dio0_pin))
