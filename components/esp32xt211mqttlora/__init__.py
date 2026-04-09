a4e46213e4ac4b5992744e19fa11d5efe1b7b765
psopso <57403373+psopso@users.noreply.github.com>
2026-04-09 11:54:59 +0200

    Update __init__.py

--- components/esp32xt211mqttlora/__init__.py
+++ components/esp32xt211mqttlora/__init__.py
@@ -3,8 +3,8 @@ import esphome.config_validation as cv
 
 from esphome.const import CONF_ID
 
-my_component_ns = cg.esphome_ns.namespace('my_component')
-MyComponent = my_component_ns.class_('MyComponent', cg.Component)
+esp32xt211mqttlora_ns = cg.esphome_ns.namespace('esp32xt211mqttlora')
+MyComponent = esp32xt211mqttlora_ns.class_('MyComponent', cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MyComponent),
