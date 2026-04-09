87bd1942c1fac34d2fd98904d46c4931aea3f3cd
psopso <57403373+psopso@users.noreply.github.com>
2026-04-09 18:34:14 +0200

    Update xt211_lora.cpp

--- components/esp32xt211mqttlora/xt211_lora.cpp
+++ components/esp32xt211mqttlora/xt211_lora.cpp
@@ -14,35 +14,6 @@ static const uint8_t REG_FRF_LSB = 0x08;
 void MyComponent::setup() {
   ESP_LOGCONFIG(TAG, "SX1278 init");
 
-  // reset pin
-  pinMode(rst_, OUTPUT);
-  digitalWrite(rst_, LOW);
-  delay(10);
-  digitalWrite(rst_, HIGH);
-  delay(10);
-
-  // přečtení verze
-  uint8_t version = read_reg(REG_VERSION);
-  ESP_LOGI(TAG, "SX1278 version: 0x%02X", version);
-
-  if (version != 0x12) {
-    ESP_LOGE(TAG, "SX1278 nenalezen!");
-    return;
-  }
-
-  // LoRa mode + sleep
-  write_reg(REG_OP_MODE, 0x80);
-
-  // Standby
-  write_reg(REG_OP_MODE, 0x81);
-
-  // frekvence 433 MHz
-  uint64_t frf = (433000000ULL << 19) / 32000000;
-
-  write_reg(REG_FRF_MSB, (uint8_t)(frf >> 16));
-  write_reg(REG_FRF_MID, (uint8_t)(frf >> 8));
-  write_reg(REG_FRF_LSB, (uint8_t)(frf >> 0));
-
   ESP_LOGI(TAG, "SX1278 init OK");
 }
 
