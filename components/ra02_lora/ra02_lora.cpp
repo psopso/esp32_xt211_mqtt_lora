59f1c7ab82b81bfbba1fab71d0de580839f91c50
psopso <57403373+psopso@users.noreply.github.com>
2026-04-17 20:10:42 +0200

    Update ra02_lora.cpp

--- components/ra02_lora/ra02_lora.cpp
+++ components/ra02_lora/ra02_lora.cpp
@@ -8,7 +8,7 @@ namespace ra02_lora {
 static const char *const TAG = "ra02_lora";
 
 void Ra02Lora::setup() {
-    gpio_install_isr_service(0);
+    //gpio_install_isr_service(0);
 
     this->spi_setup();
     this->reset_pin_->setup();
