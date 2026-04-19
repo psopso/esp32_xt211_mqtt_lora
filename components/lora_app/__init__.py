16379e5b381eae0f36d71fe94799e4e25375b654
psopso <57403373+psopso@users.noreply.github.com>
2026-04-19 18:04:53 +0200

    Update __init__.py

--- components/lora_app/__init__.py
+++ components/lora_app/__init__.py
@@ -1,6 +1,15 @@
 # lora_app/__init__.py
 import esphome.codegen as cg
 # ... (standardní importy) ...
+# Definice C++ namespace a třídy
+lora_app_ns = cg.esphome_ns.namespace('lora_app')
+Lora_App = ra02_lora_ns.class_('LoraApp', cg.Component)
+
+# Parametry pro YAML
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(LoraApp),
+}).extend(cv.COMPONENT_SCHEMA))
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
