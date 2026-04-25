b04c77854920f3e011ad75fd6314898cffc1bfc4
psopso <57403373+psopso@users.noreply.github.com>
2026-04-25 08:38:35 +0200

    Update ra02_lora.h

--- components/ra02_lora_lib/ra02_lora.h
+++ components/ra02_lora_lib/ra02_lora.h
@@ -6,9 +6,9 @@
 
 // Hardware si includuje definici rozhraní, aby ji mohl splnit
 //#include "esphome/components/xt211_lora_mqtt/ilora_driver.h"
-//#include "ilora_driver.h"
-#include "esphome/components/lora_common/ilora_driver.h"
-s
+#include "ilora_driver.h"
+//#include "esphome/components/lora_common/ilora_driver.h"
+
 //struct LoraPacket {
 //    std::vector<uint8_t> data;
 //    int16_t rssi;
