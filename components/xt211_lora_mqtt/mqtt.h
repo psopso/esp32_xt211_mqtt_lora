64b396c0caecc0c4b720bb2de7b2990c1fa88002
psopso <57403373+psopso@users.noreply.github.com>
2026-04-27 22:34:19 +0200

    Update mqtt.h

--- components/xt211_lora_mqtt/mqtt.h
+++ components/xt211_lora_mqtt/mqtt.h
@@ -7,9 +7,3 @@
 
 void send_data_item_to_mqtt(const lora_queue_item_t *item);
 
-std::string get_timestamp_string(std::time_t ts) {
-    // Převod time_t na sys_time (bod v čase)
-    auto tp = std::chrono::system_clock::from_time_t(ts);
-    // Přímé formátování do stringu
-    return std::format("{:%a %Y-%m-%d %H:%M:%S GMT}", tp);
-}
