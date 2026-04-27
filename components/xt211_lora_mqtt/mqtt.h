d3f7b0c324528300669eabf8d5e1f47c820d04e9
psopso <57403373+psopso@users.noreply.github.com>
2026-04-27 22:25:26 +0200

    Update mqtt.h

--- components/xt211_lora_mqtt/mqtt.h
+++ components/xt211_lora_mqtt/mqtt.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "lora_app.h"
 
+#include <chrono>
+#include <format>
+#include <string>
+
 void send_data_item_to_mqtt(const lora_queue_item_t *item);
 
 std::string get_timestamp_string(std::time_t ts) {
