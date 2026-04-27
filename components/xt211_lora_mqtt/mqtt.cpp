d0ee4b81b5c1a056375cbb7aa3c323ea1d23fd2a
psopso <57403373+psopso@users.noreply.github.com>
2026-04-27 10:39:12 +0200

    Update mqtt.cpp

--- components/xt211_lora_mqtt/mqtt.cpp
+++ components/xt211_lora_mqtt/mqtt.cpp
@@ -1,4 +1,6 @@
 #include "esphome/core/log.h"
+#include <memory> // Nutné pro std::unique_ptr
+#include <cstdlib> // Nutné pro free()
 
 static const char *const TAG = "mqtt";
 
@@ -6,7 +8,9 @@ namespace esphome {
 namespace lora_app {
 
   void pokus() {
-    ESP_LOGI(TAG, "Pokud v mqtt");
+    ESP_LOGI(TAG, "Pokus v mqtt");
+    std::unique_ptr<cJSON, decltype(&cJSON_Delete)> root(cJSON_CreateObject(), cJSON_Delete);
+    
   }
 
  }
