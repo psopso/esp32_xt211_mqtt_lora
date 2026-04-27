052881e044cb00ae8da9a4235b00a411951dc7bc
psopso <57403373+psopso@users.noreply.github.com>
2026-04-27 10:11:57 +0200

    Update mqtt.cpp

--- components/xt211_lora_mqtt/mqtt.cpp
+++ components/xt211_lora_mqtt/mqtt.cpp
@@ -1,8 +1,12 @@
+#include "esphome/core/log.h"
+
+static const char *const TAG = "mqtt";
 
 namespace esphome {
 namespace lora_app {
 
   void pokus() {
+    ESP_LOGI(TAG, "Pokud v mqtt");
   }
 
  }
