dda511932d0c510561eeffa5e16d66a652f7af2c
psopso <57403373+psopso@users.noreply.github.com>
2026-04-28 10:03:15 +0200

    Update mqtt.cpp

--- components/xt211_lora_mqtt/mqtt.cpp
+++ components/xt211_lora_mqtt/mqtt.cpp
@@ -48,9 +48,9 @@ std::string get_timestamp_string(std::time_t ts) {
     cJSON *values = cJSON_CreateObject();
     cJSON_AddItemToObject(data, "values", values);
     
-    cJSON_AddNumberToObject(values, "1.8.0", (double)round(item->obis_1_8_0_Wh/1000 * 100.0) / 100.0);
+    cJSON_AddNumberToObject(values, "1.8.0", (double)item->obis_1_8_0_Wh/1000.000);
     cJSON_AddNumberToObject(values, "1.8.1", (double)item->obis_1_8_1_Wh/1000.000);
-    cJSON_AddNumberToObject(values, "1.8.2", item->obis_1_8_2_Wh/1000);
+    cJSON_AddNumberToObject(values, "1.8.2", (double)item->obis_1_8_2_Wh/1000.000);
     
 
     //mqtt::global_mqtt_client->publish("muj/topic", json_string.get());
