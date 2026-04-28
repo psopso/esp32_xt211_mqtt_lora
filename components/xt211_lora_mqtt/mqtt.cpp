e093935aa4e941c4089ff91fe1bb7db33935eea6
psopso <57403373+psopso@users.noreply.github.com>
2026-04-28 22:40:14 +0200

    Update mqtt.cpp

--- components/xt211_lora_mqtt/mqtt.cpp
+++ components/xt211_lora_mqtt/mqtt.cpp
@@ -65,6 +65,6 @@ std::string get_timestamp_string(std::time_t ts) {
 //    }
     std::string base = esphome::mqtt::global_mqtt_client->get_topic_prefix();
     ESP_LOGI(TAG, "Topic1: %s", base.c_str());
-    esphome::mqtt::global_mqtt_client->publish("elektromertest1"+"/data", json_string.get());
+    esphome::mqtt::global_mqtt_client->publish("elektromertest1/data", json_string.get());
 
   }
