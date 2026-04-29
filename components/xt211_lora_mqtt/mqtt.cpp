f06eaa7e792f4194466bd2cc5127a25230519d8e
psopso <57403373+psopso@users.noreply.github.com>
2026-04-29 19:18:49 +0200

    Update mqtt.cpp

--- components/xt211_lora_mqtt/mqtt.cpp
+++ components/xt211_lora_mqtt/mqtt.cpp
@@ -62,9 +62,36 @@ std::string get_timestamp_string(std::time_t ts) {
     esphome::mqtt::global_mqtt_client->publish(topic->c_str(), json_string.get());
 
   }
-
-  void send_status_to_mqtt(const lora_queue_item_t *item, std::string *topic) {
+/*
+typedef struct {
+    uint8_t state_code;        // 1B (NapĹ?. 0 = OK, 1 = INIT, 2 = ERROR) mĂ­sto textu
+    uint8_t reason_code;       // 1B (NapĹ?. 0 = WAKEUP, 1 = REBOOT) mĂ­sto textu
+    uint32_t boot_count;       // 4B
+    uint32_t wakeup_cycle_count;// 4B
+    uint32_t first_boot_time;  // 4B
+    int32_t adaptive_offset;   // 4B
+    int16_t ntp_drift_ms;      // 2B (ntp_drift * 1000)
+    int8_t rssi;               // 1B
+    uint16_t batt_voltage_mv;  // 2B (battery_voltage * 1000)
+    uint8_t batt_soc;          // 1B (0-100 %)
+} lora_status_item_t; 
+*/
+  void send_status_to_mqtt(const lora_status_item_t *status, std::string *topic) {
     ESP_LOGI(TAG, "send_status_to_mqtt");
-//elektromertest/status {"datetime":"Sun Apr 26 16:49:58 2026","Status":{"Status":"OK","StatusText":"After //wakeup","Resets":1,"Wakeups":104,"LastWait":5,"LastWaitMin":5,"LastWaitMax":53,"LastAdaptive":-20,"FirstBootTime":"Sun 2026-04-26 08:16:51 //GMT","BuildDatetime":"2026-04-26 10:14:34","Wifi":"-70","NTPDrift":"0.00","PlannedStartTime":"16:49:35","RealStartTime":"2026-04-26 16:49:35"}}
+//elektromertest/status {"datetime":"Sun Apr 26 16:49:58 2026","Status":{"Status":"OK","StatusText":"After //wakeup","Resets":1,"Wakeups":104,"LastAdaptive":-20,"FirstBootTime":"Sun 2026-04-26 08:16:51 //GMT","BuildDatetime":"2026-04-26 10:14:34","Wifi":"-70","NTPDrift":"0.00","PlannedStartTime":"16:49:35","RealStartTime":"2026-04-26 16:49:35"}}
 
+    ESP_LOGI(TAG, "send_status_to_mqtt");
+    std::unique_ptr<cJSON, decltype(verbose_cjson_delete)> root(cJSON_CreateObject(), verbose_cjson_delete);
+    if (!root) {
+      ESP_LOGE("LORA", "Malo pameti pro JSON");
+      return; // Pokud dojde paměť, prostě vyskočíme. Žádný únik nehrozí.
+    }
+
+    cJSON_AddStringToObject(root.get(), "datetime", dtnow.c_str());
+
+    cJSON *status = cJSON_CreateObject();
+    cJSON_AddItemToObject(root.get(), "status", status);
+    std::string dtnow = get_timestamp_string(std::time(nullptr));
+    
+    
   }
