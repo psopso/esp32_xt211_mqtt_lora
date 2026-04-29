dcaab23b9359fbfafa988d7b416b2def093fd31c
psopso <57403373+psopso@users.noreply.github.com>
2026-04-29 20:30:19 +0200

    Update mqtt.h

--- components/xt211_lora_mqtt/mqtt.h
+++ components/xt211_lora_mqtt/mqtt.h
@@ -6,4 +6,4 @@
 #include <string>
 
 void send_data_item_to_mqtt(const lora_queue_item_t *item, std::string *topic);
-void send_status_to_mqtt(const lora_queue_item_t *item, std::string *topic);
+void send_status_to_mqtt(const lora_status_item_t *statusitem, std::string *topic);
