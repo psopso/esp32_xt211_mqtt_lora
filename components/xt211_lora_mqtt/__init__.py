94279bb3616d2ff5b2089e405494139e77a70319
psopso <57403373+psopso@users.noreply.github.com>
2026-09-09 17:02:07 +0200

    Update __init__.py

--- components/xt211_lora_mqtt/__init__.py
+++ components/xt211_lora_mqtt/__init__.py
@@ -3,6 +3,7 @@
 import esphome.config_validation as cv
 import esphome.codegen as cg
 from esphome.const import CONF_ID
+from esphome.components import sensor
 # Tímto importem získáme přístup k definici driveru
 from .. import ra02_lora_lib
 
