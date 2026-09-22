839dd0e79447e6c1dfd5af64e48746c1da00e83a
psopso <57403373+psopso@users.noreply.github.com>
2026-09-22 21:56:52 +0200

    Update lora_app.cpp

--- components/xt211_lora_mqtt/lora_app.cpp
+++ components/xt211_lora_mqtt/lora_app.cpp
@@ -48,7 +48,7 @@ void LoRaMqttGateway::loop() {
 	  
 	  // 3. Přetypování paměti na bajty a uložení do vektoru
 	  const uint8_t* bytes = reinterpret_cast<const uint8_t*>(&pkt);
-	  size_t real_size = 6;
+	  size_t real_size = 8;
 	  std::vector<uint8_t> response(bytes, bytes + real_size);
 //      std::vector<uint8_t> response = {0x50, 0x4F, 0x4E, 0x47};
       this->driver_->send_packet(response);
