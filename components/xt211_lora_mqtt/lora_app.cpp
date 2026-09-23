2ef8c0a0144cdf8d5e4c9e4c1761ca7c23b2dd1a
psopso <57403373+psopso@users.noreply.github.com>
2026-09-23 10:46:37 +0200

    Update lora_app.cpp

--- components/xt211_lora_mqtt/lora_app.cpp
+++ components/xt211_lora_mqtt/lora_app.cpp
@@ -41,7 +41,7 @@ void LoRaMqttGateway::loop() {
 	  pkt.packet_type = 0x03; // např. MSG_TYPE_REPLYSTATUS
 	  pkt.item_count = 1;
 	  time_t epoch_time = time(NULL);
-	  pkt->payload.lorareplystatus.ntp_time = (uint32_t)epoch_time;
+	  pkt.payload.lorareplystatus.ntp_time = (uint32_t)epoch_time;
 	  if (this->force_ota)
 		pkt.payload.lorareplystatus.replystatus	= 2;
 	  else
